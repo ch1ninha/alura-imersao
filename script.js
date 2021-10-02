d229783ded1b9768f5bfeb7b0e02415cb3308db4
Lucas Gabriel Schneider <72000637+ch1ninha@users.noreply.github.com>
2021-10-02 03:20:35 -0300

    Adicionei os primeiros arquivos
    Colocando os arquivos do .js, .html e .css

---
+++ script.js
@@ -0,0 +1,11 @@
+function temaEscuro(){
+  document.body.classList.toggle("dark")
+  var nome = document.getElementById("btn-tema-escuro")
+  console.log(nome)
+  
+}
+
+function temaEspaco(){
+  var elementoEspaco = document.getElementById("btn-tema-espaco")
+  console.log(elementoEspaco)
+}
